7b50ecab9e68d763f05d50821a207f64b9f7b23c
Mahdi Rahmani <mahdirahmani.main@gmail.com>
2024-08-22 01:10:53 +0330

    add colors to tailwind

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import './index.css'
 function App() {
   const [count, setCount] = useState(0)
 
-  return <h1 className='text-3xl font-bold'>Freelancing App</h1>
+  return <h1 className='text-3xl font-bold'>پروژه</h1>
 }
 
 export default App
