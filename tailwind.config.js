c2fcc44c1fa9200c19d2d817c1d136700376d2e6
Mahdi Rahmani <mahdirahmani.main@gmail.com>
2024-08-22 00:57:11 +0330

    add files

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,16 @@
 /** @type {import('tailwindcss').Config} */
+
+import { fontFamily } from "tailwindcss/defaultTheme";
+
 export default {
   content: ["./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ["Vazir", ...fontFamily.sans],
+      },
+    },
   },
   plugins: [],
 }
