b5398c00a8911d3ef3f17b42e5ad1e1f09607f4e
Mahdi Rahmani <mahdirahmani.main@gmail.com>
2024-08-22 04:28:38 +0330

    edit styling otp form

--- src/features/authentication/SendOTPForm.jsx
+++ src/features/authentication/SendOTPForm.jsx
@@ -15,13 +15,11 @@ function SendOTPForm() {
             onChange={(e) => setPhoneNumber(e.target.value)}
             id="phoneNumber"
             placeholder="مثال: 091211111111"
-            className="w-full py-3 px-4 rounded-xl text-secondary-900 border border-gray-300 hover:border-primary-500 focus:border-primary-500 focus:bg-white transition-all duration-300 ease-out focus:shadow-lg focus:shadow-primary-200"
+            className="textField__input"
             type="text"
           />
         </div>
-        <button className="py-2 px-4 font-bold bg-primary-900 text-white w-full rounded-xl transition-all duration-300 hover:bg-primary-800 shadow-lg shadow-primary-300">
-          ارسال کد تایید
-        </button>
+        <button className="btn btn--primary w-full">ارسال کد تایید</button>
       </form>
     </div>
   );
