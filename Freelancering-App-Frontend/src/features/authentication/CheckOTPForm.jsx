9f2d0754b6624cac38b1b94686c9df559c5399ab
Mahdi Rahmani <mahdirahmani.main@gmail.com>
2024-08-27 03:20:07 +0330

    resend otp

--- Freelancering-App-Frontend/src/features/authentication/CheckOTPForm.jsx
+++ Freelancering-App-Frontend/src/features/authentication/CheckOTPForm.jsx
@@ -1,12 +1,16 @@
 import { useMutation } from "@tanstack/react-query";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import OTPInput from "react-otp-input";
 import { checkOtp } from "../../services/authService";
 import toast from "react-hot-toast";
 import { useNavigate } from "react-router-dom";
+import { HiArrowRight } from "react-icons/hi";
 
-function CheckOTPForm({ phoneNumber }) {
+const RESEND_TIME = 90
+
+function CheckOTPForm({ phoneNumber, onBack, onReSendOtp }) {
   const [otp, setOtp] = useState("");
+  const [time, setTime] = useState(RESEND_TIME);
   const navigate = useNavigate();
   const { isPending, error, data, mutateAsync } = useMutation({
     mutationFn: checkOtp,
@@ -18,17 +22,33 @@ function CheckOTPForm({ phoneNumber }) {
       const { message, user } = await mutateAsync({ phoneNumber, otp });
       toast.success(message);
       if (user.isActive) {
-
       } else {
-        navigate('./complete-profile')
+        navigate("./complete-profile");
       }
     } catch (error) {
       toast.error(error?.response?.data?.message);
     }
   };
 
+  useEffect(() => {
+    const timer = setInterval(() => time > 0 && setTime((t) => t - 1), 1000);
+    return () => {
+      if (timer) clearInterval(timer);
+    };
+  }, [time]);
+
   return (
     <div>
+      <button onClick={onBack}>
+        <HiArrowRight className="w-6 h-6 text-secondary-500" />
+      </button>
+      <div className="mb-4 text-secondary-500">
+        {time > 0 ? (
+          <p>{time} ثانیه تا ارسال مجدد کد</p>
+        ) : (
+          <button onClick={onReSendOtp}>ارسال مجدد كد تايید</button>
+        )}
+      </div>
       <form className="space-y-10" onSubmit={checkOtpHandler}>
         <p className="font-bold text-secondary-800">کد تایید را وارد کنید :</p>
         <OTPInput
--- Freelancering-App-Frontend/src/features/authentication/SendOTPForm.jsx
+++ Freelancering-App-Frontend/src/features/authentication/SendOTPForm.jsx
@@ -5,25 +5,12 @@ import { getOtp } from "../../services/authService";
 import toast from "react-hot-toast";
 import Loading from "../../ui/Loading";
 
-function SendOTPForm({ setStep, phoneNumber, onChange }) {
-  const { isPending, error, data, mutateAsync } = useMutation({
-    mutationFn: getOtp,
-  });
+function SendOTPForm({ onSubmit,isSendingOtp, phoneNumber, onChange }) {
 
-  const sendOtpHandler = async (e) => {
-    e.preventDefault();
-    try {
-      const data = await mutateAsync({ phoneNumber });
-      setStep(2);
-      toast.success(data.message);
-    } catch (error) {
-      toast.error(error?.response?.data?.message);
-    }
-  };
 
   return (
     <div>
-      <form onSubmit={sendOtpHandler} className="space-y-8">
+      <form onSubmit={onSubmit} className="space-y-8">
         <TextField
           label="شماره موبایل :"
           name="phoneNumber"
@@ -31,7 +18,7 @@ function SendOTPForm({ setStep, phoneNumber, onChange }) {
           onChange={onChange}
         />
         <div>
-          {isPending ? (
+          {isSendingOtp ? (
             <Loading />
           ) : (
             <button type="submit" className="btn btn--primary w-full">
