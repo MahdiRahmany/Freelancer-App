cb8d41b60dd179d26a98ed63f597cd8a8b730530
Mahdi Rahmani <mahdirahmani.main@gmail.com>
2024-08-27 23:55:32 +0330

    handling refresh and access token

--- Freelancering-App-Frontend/src/App.jsx
+++ Freelancering-App-Frontend/src/App.jsx
@@ -2,16 +2,26 @@ import Auth from "./pages/Auth";
 import "./index.css";
 import { Route, Routes } from "react-router-dom";
 import CompleteProfile from "./pages/CompleteProfile";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { Toaster } from "react-hot-toast";
+import Home from "./pages/Home";
+import NotFound from "./pages/NotFound";
+
+const queryClient = new QueryClient();
 
 function App() {
   return (
-    <div className="container xl:max-w-screen-xl">
-
-      <Routes>
-        <Route path="/auth" element={<Auth />} />
-        <Route path="/complete-profile" element={<CompleteProfile />} />
-      </Routes>
-    </div>
+    <QueryClientProvider client={queryClient}>
+      <Toaster />
+      <div className="container xl:max-w-screen-xl">
+        <Routes>
+          <Route path="/auth" element={<Auth />} />
+          <Route path="/complete-profile" element={<CompleteProfile />} />
+          <Route path="/" element={<Home />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </div>
+    </QueryClientProvider>
   );
 }
 export default App;
