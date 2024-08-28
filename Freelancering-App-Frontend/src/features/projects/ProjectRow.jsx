44e36a50a30e912aeaf07297fb8445c4b3871db6
Mahdi Rahmani <mahdirahmani.main@gmail.com>
2024-08-29 02:13:07 +0330

    modal markup

--- Freelancering-App-Frontend/src/features/projects/ProjectRow.jsx
+++ Freelancering-App-Frontend/src/features/projects/ProjectRow.jsx
@@ -1,9 +1,14 @@
+import { TbPencilMinus } from "react-icons/tb";
 import Table from "../../ui/Table";
 import toLocaleDateShort from "../../utils/toLocalDateShort";
 import { toPersianNumbersWithComma } from "../../utils/toPersianNumbers";
 import truncateText from "../../utils/truncateText";
+import { HiOutlineTrash } from "react-icons/hi";
+import Modal from "../../ui/Modal";
+import { useState } from "react";
 
 function ProjectRow({ project, index }) {
+  const [isEditOpen, setIsEditOpen] = useState(false);
   return (
     <Table.Row>
       <td>{index + 1}</td>
@@ -20,7 +25,21 @@ function ProjectRow({ project, index }) {
       <td>
         {project.status === "OPEN" ? <span>باز</span> : <span>بسته</span>}
       </td>
-      <td>...</td>
+      <td>
+        <div className="flex items-center gap-x-4">
+          <button onClick={() => setIsEditOpen(true)}>
+            <TbPencilMinus className="w-5 h-5 text-primary-900" />
+          </button>
+          <Modal
+            title="Modal Title"
+            open={isEditOpen}
+            onClose={() => setIsEditOpen(false)}
+          ></Modal>
+          <button>
+            <HiOutlineTrash className="w-5 h-5 text-error" />
+          </button>
+        </div>
+      </td>
     </Table.Row>
   );
 }
