fe02b8fc154610a9f8e1123a8a77936b980351f4
Mahdi Rahmani <mahdirahmani.main@gmail.com>
2024-08-30 23:33:20 +0330

    user logout

---
+++ Freelancering-App-Frontend/src/ui/DarkThemeToggle.jsx
@@ -0,0 +1,10 @@
+import { HiOutlineSun } from "react-icons/hi";
+
+function DarkThemeToggle() {
+  return (
+    <button>
+      <HiOutlineSun className="w-5 h-5 text-primary-900" />
+    </button>
+  );
+}
+export default DarkThemeToggle;
