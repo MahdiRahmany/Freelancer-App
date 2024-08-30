5a5087b58fd4f3d10c75ca14c2c72c5c44c24d81
Mahdi Rahmani <mahdirahmani.main@gmail.com>
2024-08-31 00:08:01 +0330

    dark mode on project

--- Freelancering-App-Frontend/src/features/authentication/UserAvatar.jsx
+++ Freelancering-App-Frontend/src/features/authentication/UserAvatar.jsx
@@ -5,7 +5,7 @@ function UserAvatar() {
   return (
     <div className="flex items-center gap-x-2 text-secondary-600">
       <img
-        src="/public/freelancering.svg"
+        src="/public/user.jpg"
         alt="user-account"
         className="w-7 h-7 rounded-full object-cover object-center"
       />
