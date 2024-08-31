2d44c34f10e31b013c088f6bda31c06dde35cc1f
Mahdi Rahmani <mahdirahmani.main@gmail.com>
2024-08-31 23:16:32 +0330

    filter drop down

--- Freelancering-App-Frontend/src/features/freelancer/projects/ProjectsHeader.jsx
+++ Freelancering-App-Frontend/src/features/freelancer/projects/ProjectsHeader.jsx
@@ -1,8 +1,24 @@
+import { RxValue } from "react-icons/rx";
+import useCategories from "../../../hooks/useCategories";
+import FilterDropDown from "../../../ui/FilterDropDown";
+
 function ProjectsHeader() {
+  const { transformCategories } = useCategories();
   return (
     <div className="flex items-center justify-between text-secondary-700 mb-8">
       <h1 className="text-lg font-bold">لیست پروژه ها</h1>
-      <div>فیلتر ها</div>
+      <div>
+        <FilterDropDown
+          filterField="category"
+          options={[
+            {
+              value: "ALL",
+              label: "دسته بندی (همه)",
+            },
+            ...transformCategories,
+          ]}
+        />
+      </div>
     </div>
   );
 }
