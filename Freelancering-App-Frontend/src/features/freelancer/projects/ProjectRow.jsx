439ffa5d9a13bc3d113113f8760fcf391014a7d6
Mahdi Rahmani <mahdirahmani.main@gmail.com>
2024-08-31 22:09:58 +0330

    create proposal form

--- Freelancering-App-Frontend/src/features/freelancer/projects/ProjectRow.jsx
+++ Freelancering-App-Frontend/src/features/freelancer/projects/ProjectRow.jsx
@@ -5,8 +5,10 @@ import truncateText from "../../utils/truncateText";
 import { useState } from "react";
 import { useRemoveProject } from "./useRemoveProject";
 import { MdAssignmentAdd } from "react-icons/md";
+import Modal from "../../../ui/Modal";
+import CreateProposal from "../../proposals/CreateProposal";
 
-const status = {
+const projectStatus = {
   OPEN: {
     label: "باز",
     className: "badge--success",
@@ -18,20 +20,31 @@ const status = {
 };
 
 function ProjectRow({ project, index }) {
-  const { status } = project;
+  const { status, budget, title, deadline } = project;
+  const [open, setOpen] = useState(false);
 
   return (
     <Table.Row>
       <td>{index + 1}</td>
-      <td>{truncateText(project.title, 30)}</td>
-      <td>{toPersianNumbersWithComma(project.budget)}</td>
-      <td>{toLocaleDateShort(project.deadline)}</td>
+      <td>{truncateText(title, 30)}</td>
+      <td>{toPersianNumbersWithComma(budget)}</td>
+      <td>{toLocaleDateShort(deadline)}</td>
       <td>
         <span className={`badge ${projectStatus[status].className}`}>
           {projectStatus[status].label}
         </span>
       </td>
       <td>
+        <Modal
+          open={open}
+          onClose={() => setOpen(false)}
+          title={`درخواست انجام پروژه ${title}`}
+        >
+          <CreateProposal
+            projectId={project._id}
+            onClose={() => setOpen(false)}
+          />
+        </Modal>
         <button>
           <MdAssignmentAdd className="w-5 h-5 text-primary-900" />
         </button>
