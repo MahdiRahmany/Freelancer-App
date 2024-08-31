28e45b515aa62847ae16b624dfbd967199b9916e
Mahdi Rahmani <mahdirahmani.main@gmail.com>
2024-09-01 02:57:32 +0330

    protected route

--- Freelancering-App-Frontend/src/ui/ProtectedRoute.jsx
+++ Freelancering-App-Frontend/src/ui/ProtectedRoute.jsx
@@ -1,7 +1,24 @@
+import { useNavigate } from "react-router-dom";
 import useAuthorize from "../features/authentication/useAuthorize";
+import { useEffect } from "react";
+import Loading from "./Loading";
 
 function ProtectedRoute({ children }) {
   const { isAuthenticated, isLoading, isAuthorized } = useAuthorize();
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (!isAuthenticated && !isLoading) navigate("/");
+    if (!isAuthorized && !isLoading)
+      navigate("/note-access", { replace: true });
+  }, [isAuthenticated, isAuthorized, isLoading, navigate]);
+
+  if (isLoading)
+    return (
+      <div className="flex items-center justify-between h-screen bg-secondary-100">
+        <Loading />
+      </div>
+    );
   return children;
 }
 export default ProtectedRoute;
