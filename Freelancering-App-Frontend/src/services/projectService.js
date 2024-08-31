2dedd4116309b585dc28ea90bbd80f63d2c49232
Mahdi Rahmani <mahdirahmani.main@gmail.com>
2024-09-01 01:32:23 +0330

    queryString for filtering

--- Freelancering-App-Frontend/src/services/projectService.js
+++ Freelancering-App-Frontend/src/services/projectService.js
@@ -1,31 +1,36 @@
 import http from "./httpService";
 
 export function getOwnerProjectsApi() {
-  return http.get("/project/owner-projects").then((data) => data.data);
+  return http.get("/project/owner-projects").then(({ data }) => data.data);
 }
 
 export function removeProjectApi(id) {
-  return http.delete(`/project/${id}`).then((data) => data.data);
+  return http.delete(`/project/${id}`).then(({ data }) => data.data);
 }
 
 export function createProjectApi(data) {
-  return http.post("/project/add", data).then((data) => data.data);
+  return http.post("/project/add", data).then(({ data }) => data.data);
 }
 
 export function editProjectApi({ id, newProject }) {
   return http
     .patch(`/project/update/${id}`, newProject)
-    .then((data) => data.data);
+    .then(({ data }) => data.data);
 }
 
 export function toggleProjectStatusApi({ id, newProject }) {
-  return http.patch(`/project/${id}`, newProject).then((data) => data.data);
+  return http.patch(`/project/${id}`, newProject).then(({ data }) => data.data);
 }
 
 export function getProjectApi({ id }) {
-  return http.get(`/project/${id}`).then((data) => data.data);
+  return http.get(`/project/${id}`).then(({ data }) => data.data);
 }
 
-export function getProjectsApi() {
-  return http.get("/project/list").then((data) => data.data);
+export function getProjectsApi(id) {
+  return http.get(`/project/${id}`).then(({ data }) => data.data);
 }
+
+export function getProjectsApi(qs) {
+  return http.get(`/project/list${qs}`).then(({ data }) => data.data);
+}
+ 
