1cd0a50bd51f9d7fac61903ac794b111839cda50
Mahdi Rahmani <mahdirahmani.main@gmail.com>
2024-09-01 04:41:08 +0330

    projects and proposals routes

--- Freelancering-App-Frontend/src/pages/Proposals.jsx
+++ Freelancering-App-Frontend/src/pages/Proposals.jsx
@@ -3,8 +3,10 @@ import ProposalTable from "../features/proposals/proposalTable";
 function Proposals() {
   return (
     <div>
-      <h1 className="font-black text-secondary-700 text-xl mb-8">پروپوزال های شما</h1>
-      <ProposalTable/>
+      <h1 className="font-black text-secondary-700 text-xl mb-8">
+        لیست پروپوزال ها
+      </h1>
+      <ProposalTable />
     </div>
   );
 }
